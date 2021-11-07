62bb587c9e680fac2b6ded1da25715742a084843
Brian Gamble <brianjgamble@users.noreply.github.com>
2021-11-07 15:02:32 -0600

    Handling failure for post /expense unit tests

--- app/api.rb
+++ app/api.rb
@@ -1,4 +1,5 @@
 require 'roda'
+require_relative 'ledger'
 
 module ExpenseTracker
   class API < Roda
@@ -13,7 +14,13 @@ module ExpenseTracker
         r.post do
           expense = JSON.parse(request.body.read)
           result = @@ledger.record(expense)
-          {expense_id: result.expense_id}
+
+          if result.success?
+            {expense_id: result.expense_id}
+          else
+            response.status = 422
+            {'error' => result.error_message}
+          end
         end
 
         r.get String do |date|
